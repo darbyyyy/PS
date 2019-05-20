876ba8966637ddea0310361d6165af239991fc17
Jaeyong Park <darbyyyy@gmail.com>
2019-05-20 11:50:23 +0900

    Study_20190524

---
+++ 20190524/1249.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <queue>
+#include <utility>
+
+using namespace std;
+
+int main(){
+	int T, N, map[100][100], cost[100][100];
+	char input;
+	queue <pair<int, int>> q;
+	pair<int, int> coord;	// coord.first - row , coord.second - column
+
+	cin >> T;
+
+	for(int test_case=1; test_case<=T; test_case++){
+
+		cin >> N;
+		for(int i = 0 ; i < N ; i ++){
+			for(int j = 0 ; j < N ; j++) {
+				cin>>input;
+				map[i][j] = input - '0';
+				cost[i][j]=-1;
+			}
+		}
+
+		q.push(make_pair(0, 0));
+		cost[0][0]=0;
+
+		while(!q.empty()){
+			coord = q.front(); q.pop();
+			int x = coord.first, y = coord.second;
+
+			//간소화도 가능하지만 귀찮다.
+			if((x-1 >=0) &&  ((cost[x-1][y] == -1) || (cost[x-1][y] > cost[x][y]+ map[x-1][y])) )	//left dir
+				{cost[x-1][y] = cost[x][y] + map[x-1][y]; q.push(make_pair(x-1, y));				}
+			if((x+1 <N)  &&  ((cost[x+1][y] == -1) || (cost[x+1][y] > cost[x][y]+ map[x+1][y])) )	//right dir
+				{cost[x+1][y] = cost[x][y] + map[x+1][y]; q.push(make_pair(x+1, y));}
+			if((y-1 >=0) &&  ((cost[x][y-1] == -1) || (cost[x][y-1] > cost[x][y]+ map[x][y-1])) )	//up dir
+				{cost[x][y-1] = cost[x][y] + map[x][y-1]; q.push(make_pair(x, y-1));}
+			if((y+1 <N)  &&  ((cost[x][y+1] == -1) || (cost[x][y+1] > cost[x][y]+ map[x][y+1])) ) 	//down dir
+				{cost[x][y+1] = cost[x][y] + map[x][y+1]; q.push(make_pair(x, y+1));}
+		}
+
+		cout << '#' << test_case << ' ' << cost[N-1][N-1] << endl;
+	}
+	return 0;
+}
---
+++ 20190524/5658.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+int main(){
+	int T, answer, N, K, length, to_decimal, ints[48];
+	char input[48];
+	vector <int> numbers;
+	cin >> T;
+
+	for(int test_case =1; test_case<=T; test_case++){
+		cin >> N >> K;
+		length = N / 4;
+		for(int i = 0 ; i < N ; i++){		//Chars into ints
+			cin>>input[i];
+			if(input[i] >= 'A')
+				ints[i] = input[i]-'A' +10;
+			else
+				ints[i] = input[i]-'0';
+		}
+
+		for(int itr = 0 ; itr < length; itr++){	//hexadecimal into decimal
+			for(int i = 0 ; i < 4; i++){
+				to_decimal = ints[length*i+itr];
+				for(int j = 1 ; j < length; j++){
+					to_decimal *= 16;
+					to_decimal+= ints[(length*i + itr + j)%N];
+				}
+				numbers.push_back(to_decimal);
+			}
+		}
+		sort(numbers.begin(), numbers.end());
+
+		set<int> s( numbers.begin(), numbers.end() );
+		numbers.assign( s.begin(), s.end() );
+
+
+		cout << "#" << test_case << " "<<numbers[numbers.size() - K]<<endl;
+		numbers.clear();
+	}
+	return 0;
+
+}
